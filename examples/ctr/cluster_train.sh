7cf1ca58978338af395f89e59f206e63326900a2
guru4elephant <guru4elephant@gmail.com>
2019-06-17 21:02:20 +0800

    add ctr scripts with fleet api
    Former-commit-id: 06a684626375a0c9fa680f60389e8ce78aba18d3
    Former-commit-id: 20b1d49d2f50bbbd0a29fe30cb1830bd87db17cc

---
+++ examples/ctr/cluster_train.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# start pserver0
+python dist_ctr.py \
+    --is_local 0 \
+    --role pserver \
+    --endpoints 127.0.0.1:6000,127.0.0.1:6001 \
+    --current_endpoint 127.0.0.1:6000 \
+    --trainers 2 \
+    > pserver0.log 2>&1 &
+
+# start pserver1
+python dist_ctr.py \
+    --is_local 0 \
+    --role pserver \
+    --endpoints 127.0.0.1:6000,127.0.0.1:6001 \
+    --current_endpoint 127.0.0.1:6001 \
+    --trainers 2 \
+    > pserver1.log 2>&1 &
+
+# start trainer0
+python dist_ctr.py \
+    --is_local 0 \
+    --role trainer \
+    --endpoints 127.0.0.1:6000,127.0.0.1:6001 \
+    --trainers 2 \
+    --trainer_id 0 \
+    > trainer0.log 2>&1 &
+
+# start trainer1
+python dist_ctr.py \
+    --is_local 0 \
+    --role trainer \
+    --endpoints 127.0.0.1:6000,127.0.0.1:6001 \
+    --trainers 2 \
+    --trainer_id 1 \
+    > trainer1.log 2>&1 &
