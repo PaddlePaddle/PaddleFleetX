b124bad588aabd8e876deec29d3876446b97dfc5
guru4elephant <guru4elephant@gmail.com>
2019-06-23 12:12:49 +0800

    add benchmark scripts for ctr

---
+++ benchmark/ps/ctr/local_train.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+
+from args import parse_args
+import os
+import paddle.fluid as fluid
+import sys
+from network_conf import ctr_dnn_model_dataset
+
+dense_feature_dim = 13
+
+def train():
+    args = parse_args()
+    if not os.path.isdir(args.model_output_dir):
+        os.mkdir(args.model_output_dir)
+    
+    dense_input = fluid.layers.data(
+        name="dense_input", shape=[dense_feature_dim], dtype='float32')
+    sparse_input_ids = [
+        fluid.layers.data(name="C" + str(i), shape=[1], lod_level=1, dtype="int64")
+        for i in range(1, 27)]
+    label = fluid.layers.data(name='label', shape=[1], dtype='int64')
+
+    loss, auc_var, batch_auc_var = ctr_dnn_model_dataset(dense_input, sparse_input_ids, label,
+                                                         args.embedding_size, args.sparse_feature_dim)
+
+    optimizer = fluid.optimizer.Adagrad(learning_rate=1e-2)
+    optimizer.minimize(loss)
+
+    exe = fluid.Executor(fluid.CPUPlace())
+    exe.run(fluid.default_startup_program())
+    dataset = fluid.DatasetFactory().create_dataset()
+    dataset.set_use_var([dense_input] + sparse_input_ids + [label])
+    pipe_command = "python criteo_reader.py %d" % args.sparse_feature_dim
+    dataset.set_pipe_command(pipe_command)
+    dataset.set_batch_size(100)
+    thread_num = 11
+    dataset.set_thread(thread_num)
+    whole_filelist = ["raw_data/part-%d" % x for x in range(len(os.listdir("raw_data")))]
+    dataset.set_filelist(whole_filelist)
+    epochs = 20
+    for i in range(epochs):
+        exe.train_from_dataset(program=fluid.default_main_program(),
+                               dataset=dataset,
+                               fetch_list=[auc_var],
+                               fetch_info=["auc"],
+                               print_period=1000,
+                               debug=True)
+        model_dir = args.model_output_dir + '/epoch' + str(i + 1) + ".model"
+        sys.stderr.write("epoch%d finished" % (i + 1))
+        fluid.io.save_inference_model(model_dir, [dense_input.name] + [x.name for x in sparse_input_ids] + [label.name], [loss, auc_var], exe)
+
+if __name__ == '__main__':
+    train()
