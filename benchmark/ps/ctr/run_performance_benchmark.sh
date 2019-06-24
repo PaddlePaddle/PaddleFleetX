6df73c76bd6346e9bd5456d2de978411322172ce
guru4elephant <guru4elephant@gmail.com>
2019-06-24 20:06:36 +0800

    update performance benchmark scripts
    Former-commit-id: dc503eeae148c1a2204bb235053e95b6c79a5249
    Former-commit-id: f48e16ef24ee0e12de0825677cf4829a052da20e

--- benchmark/ps/ctr/run_performance_benchmark.sh
+++ benchmark/ps/ctr/run_performance_benchmark.sh
@@ -1,6 +1,6 @@
 for thr in 40 ;
 do 
-    for bs in 128 256 512 1024 ;
+    for bs in 32 64 128 256 512 1024 ;
     do
 	python local_train.py --model_output_dir ${thr}_${bs}_models --num_passes 20 --thread_num $thr --batch_size $bs
     done
