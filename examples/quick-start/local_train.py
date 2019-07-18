fbea0777b76c6d97a565b8b6bd5e2945be6de313
guru4elephant <guru4elephant@gmail.com>
2019-07-18 19:48:07 +0800

    refine quick start examples

--- examples/quick-start/local_train.py
+++ examples/quick-start/local_train.py
@@ -1,18 +1,33 @@
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import paddle.fluid as fluid
+from nets import mlp
+from utils import gen_data
 
-input_x = fluid.layers.data()
-input_y = fluid.layers.data()
+input_x = fluid.layers.data(name="x", shape=[32], dtype='float32')
+input_y = fluid.layers.data(name="y", shape=[1], dtype='int64')
 
-fc_1 = fluid.layers.fc(input=input_x)
-fc_2 = fluid.layers.fc(input=fc_1)
-prediction = fluid.layers.fc(input=[fc_2])
-cost = fluid.layers.cross_entropy(input=prediction, label=input_y)
-optimizer = fluid.optimizer.Adagrad(learning_rate=0.01)
+cost = mlp(input_x, input_y)
+optimizer = fluid.optimizer.SGD(learning_rate=0.01)
 optimizer.minimize(cost)
 place = fluid.CPUPlace()
 
-pass_num = 10
-for pass_id in range(pass_num):
-    for x in xx():
-        
+exe = fluid.Executor(place)
+exe.run(fluid.default_startup_program())
+step = 1001
+for i in range(step):
+    exe.run(feed=gen_data())
+
 
