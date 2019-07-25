397734cecfc39d58864177d197a6d41555c33f28
lilong12 <lilong12@baidu.com>
2019-07-25 19:20:15 +0800

    Using one command to run training locally or on paddlecloud
    Former-commit-id: 3d74fdf61d19e359aeb78c68ea9ff6cf98bff0f8
    Former-commit-id: 499f6a1988354a806d34fd89dc8533012dc72922

--- benchmark/collective/resnet/scripts/train_gpu_mp.sh
+++ benchmark/collective/resnet/scripts/train_gpu_mp.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-
-export PATH=~/software/python/bin:$PATH
 set -xe
+
+if [ $PADDLE_TRAINERS ]; then
+    echo "PADDLE_TRAINERS = $PADDLE_TRAINERS"
+else
+    PADDLE_TRAINERS="127.0.0.1"
+    echo "PADDLE_TRAINERS = $PADDLE_TRAINERS"
+fi
+
+if [ $POD_IP ]; then
+    echo "POD_IP = $POD_IP"
+else
+    POD_IP="127.0.0.1"
+    echo "POD_IP = $POD_IP"
+fi
 
 export FLAGS_sync_nccl_allreduce=0
 export FLAGS_eager_delete_tensor_gb=0
@@ -9,10 +21,6 @@ export FLAGS_fraction_of_gpu_memory_to_use=0.84
 export NCCL_DEBUG=INFO
 
 # Change your training arguments:
-python -m paddle.distributed.launch ./train_with_pe_fleet.py \
-       --data_dir /ssd2/lilong/ImageNet --start_test_pass 0
-
-# For paddlecloud
-# python -m paddle.distributed.launch --cluster_node_ips=$PADDLE_TRAINERS \
-#     --node_ip=$POD_IP ./train_with_pe_fleet.py \
-#     --data_dir /ssd2/lilong/ImageNet --start_test_pass 0
+python -m paddle.distributed.launch --cluster_node_ips=$PADDLE_TRAINERS \
+    --node_ip=$POD_IP ./train_with_pe_fleet.py \
+    --data_dir /ssd2/lilong/ImageNet --start_test_pass 0
