472a825a0683a6dedebbad71418a68192cc15940
danleifeng <52735331+danleifeng@users.noreply.github.com>
2019-08-13 20:15:26 +0800

    add FleetAPI support Resnet50 and VGG16 (#48)
     add FleetAPI support Resnet50 and VGG16
    
    Former-commit-id: b3538573fce9834deca3e0db6743e8b6e26a9f5d
    Former-commit-id: 71882f660512273513cfe17ecf5b2073a549d490

--- benchmark/collective/resnet/README.md
+++ benchmark/collective/resnet/README.md
@@ -1,6 +1,24 @@
-# ResNet50 on ImageNet with Fleet APIs
-## How to Run
-sh scripts/train_gpu_mp.sh
+# ResNet50 and VGG16 on ImageNet with Fleet APIs
+## How to change different model
+you can change Resnet50 or VGG16 model easily by editing scripts/train_gpu.sh:
+```
+MODEL=ResNet50  
+# MODEL=VGG16
+```
 
-## Warning
-The current implementation can only work with paddle-1.5. I will remove this constraint as soon as possible.
+## How to run
+### with single gpu single card
+```
+scripts/train_gpu.sh:
+set NUM_CARDS=1
+
+sh scripts/train_gpu.sh
+```
+
+### with single gpu multiple cards
+```
+scripts/train_gpu.sh:
+set NUM_CARDS=8 (your gpu cards number)
+
+sh scripts/train_gpu.sh
+```
---
+++ benchmark/collective/resnet/scripts/train_gpu.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+export FLAGS_sync_nccl_allreduce=1
+export FLAGS_cudnn_exhaustive_search=0
+
+export GLOG_v=1
+export GLOG_logtostderr=1
+export FLAGS_eager_delete_tensor_gb=0
+export NCCL_DEBUG=INFO
+# Unset proxy
+unset https_proxy http_proxy
+
+set -xe
+
+MODEL=ResNet50 #VGG16
+MODEL_SAVE_PATH="output/"
+
+# training params
+NUM_EPOCHS=90
+BATCH_SIZE=32
+LR=0.001
+LR_STRATEGY=piecewise_decay
+
+# data params
+DATA_PATH="./ImageNet"
+TOTAL_IMAGES=1281167
+CLASS_DIM=1000
+IMAGE_SHAPE=3,224,224
+
+
+#gpu params
+FUSE=True
+NCCL_COMM_NUM=1
+NUM_THREADS=3
+USE_HIERARCHICAL_ALLREDUCE=False
+NUM_CARDS=1
+FP16=False #whether to use float16 
+
+if [[ ${FUSE} == "True" ]]; then
+    export FLAGS_fuse_parameter_memory_size=16 #MB
+    export FLAGS_fuse_parameter_groups_size=50
+fi
+distributed_args=""
+if [[ ${NUM_CARDS} == "1" ]]; then
+    distributed_args="--selected_gpus 0"
+fi
+
+set -x
+
+python -m paddle.distributed.launch ${distributed_args} --log_dir log \
+       ./train_with_fleet.py \
+       --model=${MODEL} \
+       --batch_size=${BATCH_SIZE} \
+       --total_images=${TOTAL_IMAGES} \
+       --data_dir=${DATA_PATH} \
+       --class_dim=${CLASS_DIM} \
+       --image_shape=${IMAGE_SHAPE} \
+       --model_save_dir=${MODEL_SAVE_PATH} \
+       --with_mem_opt=False \
+       --lr_strategy=${LR_STRATEGY} \
+       --lr=${LR} \
+       --num_epochs=${NUM_EPOCHS} \
+       --l2_decay=1e-4 \
+       --scale_loss=1.0 \
+       --fuse=${FUSE} \
+       --num_threads=${NUM_THREADS} \
+       --nccl_comm_num=${NCCL_COMM_NUM} \
+       --use_hierarchical_allreduce=${USE_HIERARCHICAL_ALLREDUCE} \
+       --fp16=${FP16}
