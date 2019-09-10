f112e85f969af10b2b94cfd4c9d3e61ad4341280
lilong12 <lilong12@baidu.com>
2019-09-10 17:40:11 +0800

    run run.sh locally

--- examples/dist_classify/run.sh
+++ examples/dist_classify/run.sh
@@ -1,21 +1,14 @@
-export PATH=~/paddle_distfc/python/bin:$PATH
-if [ $PADDLE_TRAINERS ];then
-   config="--cluster_node_ips=${PADDLE_TRAINERS} --node_ip=${POD_IP} "
-else
-    config=" "
-fi
-selected_gpus="0,1,2,3,4,5,6,7"
-
 export FLAGS_cudnn_exhaustive_search=true 
 export FLAGS_fraction_of_gpu_memory_to_use=0.96
 export FLAGS_eager_delete_tensor_gb=0.0
+selected_gpus="0,1,2,3,4,5,6,7"
 
-python -m paddle.distributed.launch $config \
+python -m paddle.distributed.launch \
   --selected_gpus $selected_gpus \
-  --log_dir mylog_distsoftmax \
+  --log_dir mylog \
   do_train.py \
   --model=ResNet_ARCFACE50 \
   --loss=softmax \
   --margin=0.5 \
   --train_batch_size 128 \
-  --with_test=False
+  --with_test=True
