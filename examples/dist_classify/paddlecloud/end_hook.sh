c0557c42d3d538cf6a0ef5a3866aa45047b482a5
lilong12 <lilong12@baidu.com>
2019-09-12 10:04:36 +0800

    add paddlecloud

---
+++ examples/dist_classify/paddlecloud/end_hook.sh
@@ -0,0 +1,7 @@
+rm -rf ImageNet
+echo "PADDLE_TRAINERS:"
+echo ${PADDLE_TRAINERS}
+echo "POD_IP:"
+echo ${POD_IP}
+
+echo "end"
---
+++ examples/dist_classify/paddlecloud/run.py
@@ -0,0 +1,6 @@
+#!/bin/env python
+# -*- encoding:utf-8 -*-
+
+if __name__ == '__main__':
+    print("Done!")
+
