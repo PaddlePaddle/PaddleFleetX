35acdc28787ec75793125009d21e4aaa1a4a8d84
malin10 <malin10@baidu.com>
2019-09-26 20:26:40 +0800

    benchmark for word2vec

---
+++ benchmark/ps/distribute_word2vec/local_dist.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export PADDLE_PSERVERS_IP_PORT_LIST=127.0.0.1:36001,127.0.0.1:36002,127.0.0.1:36003,127.0.0.1:36004
+export PADDLE_WORKERS_IP_PORT_LIST=127.0.0.1:36006,127.0.0.1:36007,127.0.0.1:36008,127.0.0.1:36009
+trainer_nums=4
+pserver_nums=4
+
+mode=$1
+if [ $mode == "sync" ]; then
+    sync=True
+else
+    sync=False
+fi
+
+for((i=0;i<${pserver_nums};i++)) 
+do
+    export TRAINING_ROLE=PSERVER
+    export PADDLE_TRAINER_ID=$i
+    python -u word2vec_distribute.py --sync_mode=${sync} &> ./log/${mode}/pserver.$i.log &
+done
+
+for((i=0;i<${trainer_nums};i++))
+do
+    export TRAINING_ROLE=TRAINER
+    export PADDLE_TRAINER_ID=$i
+    python -u word2vec_distribute.py --sync_mode=${sync} &> ./log/${mode}/worker.$i.log &
+done
