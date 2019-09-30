9dcca493bb0fc3f9003591bed0fa359e046214a4
lilong12 <lilong12@baidu.com>
2019-09-30 11:49:58 +0800

    remove commented statements
    Former-commit-id: 0bea6c2ffb70676b7c8a098826949292cce05736
    Former-commit-id: 61921020b6244aff1d8ab5f9db4e7b6eabf0ac47

--- examples/fast_imagenet/env.py
+++ examples/fast_imagenet/env.py
@@ -6,17 +6,9 @@ def dist_env():
     Return a dict of all variable that distributed training may use.
     NOTE: you may rewrite this function to suit your cluster environments.
     """
-    # trainer_id = int(os.getenv("PADDLE_TRAINER_ID", "0"))
-    # num_trainers = int(os.getenv("PADDLE_TRAINERS_NUM", "1"))
-
-    # - PADDLE_TRAINER_ENDPOINTS means nccl2 mode.
-    # trainer_endpoints = os.getenv("PADDLE_TRAINER_ENDPOINTS")
-    # trainer_endpoints = trainer_endpoints.split(',')
-    # current_endpoint = os.getenv("PADDLE_CURRENT_ENDPOINT")
-
     # For launch.py
     trainer_id = int(os.getenv("PADDLE_TRAINER_ID", "0"))
-    num_trainers = int(os.getenv("PADDLE_TRAINERS_NUM", "0"))
+    num_trainers = int(os.getenv("PADDLE_TRAINERS_NUM", "1"))
     current_endpoint = os.getenv("PADDLE_CURRENT_ENDPOINT", "127.0.0.1:6170")
     trainer_endpoints = os.getenv("PADDLE_TRAINER_ENDPOINTS", "127.0.0.1:6170")
     trainer_endpoints = trainer_endpoints.split(',')
