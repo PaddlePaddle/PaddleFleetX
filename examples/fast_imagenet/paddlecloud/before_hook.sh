8fd9f105c726dfebc9e8e117bc4581e3f18e932c
lilong12 <lilong12@baidu.com>
2019-10-08 10:08:52 +0800

    use afs instead of downloading and modify SCALE_LOSS to 64.0
    Former-commit-id: c5849c367ff645f3a101812ff90c58f2ab88c7e8
    Former-commit-id: b07d645aff79de549f2033b2f21a406334858cc2

--- examples/fast_imagenet/paddlecloud/before_hook.sh
+++ examples/fast_imagenet/paddlecloud/before_hook.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
 echo "==============JOB BEGIN============"
 
-# User configurations
-HADOOP_FS_NAME=afs://xingtian.afs.baidu.com:9902
-HADOOP_UGI=Paddle_Data,Paddle_gpu@2017
-
-echo "Show network interfaces on this machine:"
+echo "Network Interfaces:"
 ifconfig
 
+echo "GPU Info:"
+nvidia-smi
+
 echo "fetch gcc..."
-hadoop fs -D fs.default.name=${HADOOP_FS_NAME} -D hadoop.job.ugi=${HADOOP_UGI} -get ./lilong/gcc-4.8.2.tar.gz ./
 mkdir /opt/compiler
-tar zxf gcc-4.8.2.tar.gz 
+tar zxf ./afs/gcc-4.8.2.tar.gz
 mv gcc-4.8.2 /opt/compiler
-rm gcc-4.8.2.tar.gz
 
 echo "fetch python..."
-hadoop fs -D fs.default.name=${HADOOP_FS_NAME} -D hadoop.job.ugi=${HADOOP_UGI} -get ./lilong/python_latest.tar.gz ./
-echo "untar..."
-tar zxf python_latest.tar.gz 
-rm python_latest.tar.gz
+tar zxf ./afs/python_latest.tar.gz
 
 echo "fetch data..."
-#export PATH=/root/paddlejob/hadoop-client/hadoop/bin:$PATH
 tar zxf ./afs/fast_resnet_data.tar.gz
 cp ./afs/sorted_idxar.p ./fast_resnet_data/
 
@@ -39,11 +32,12 @@ mv fp16_utils.py learning_rate.py utils
 mv __init__.py utils
 
 export NCCL_DEBUG=INFO
-export FLAGS_fraction_of_gpu_memory_to_use=0.9
+export FLAGS_fraction_of_gpu_memory_to_use=0.96
 export FLAGS_sync_nccl_allreduce=1
 export FLAGS_eager_delete_tensor_gb=0.0
 export FLAGS_cudnn_exhaustive_search=1
-export FLAGS_fuse_parameter_memory_size=16 #MB
+#export FLAGS_fuse_parameter_memory_size=16
+export FLAGS_fuse_parameter_memory_size=30
 export FLAGS_fuse_parameter_groups_size=50
 
 export PATH=`pwd`/python/bin:$PATH
@@ -55,10 +49,9 @@ else
     config=" "
 fi
 
-
 FP16=True
 if [ "${FP16}" = "True" ]; then
-    SCALE_LOSS=128.0
+    SCALE_LOSS=64.0
 else
     SCALE_LOSS=1.0
 fi
@@ -67,9 +60,30 @@ python -m paddle.distributed.launch ${config} \
   --selected_gpus="0,1,2,3,4,5,6,7" \
   --log_dir=${LOGDIR} \
   train.py --data_dir=./fast_resnet_data/ \
-  --num_epochs=40 --fp16=${FP16} \
+  --num_epochs=28 \
+  --fp16=${FP16} \
   --scale_loss=${SCALE_LOSS} \
-  --start_test_pass=0 --log_period=100 \
+  --start_test_pass=26 \
+  --log_period=100 \
   --fuse=True \
   --profile=False 
-
+  
+FP16=False
+if [ "${FP16}" = "True" ]; then
+    SCALE_LOSS=64.0
+else
+    SCALE_LOSS=1.0
+fi
+LOGDIR="mylog_fp${FP16}"
+python -m paddle.distributed.launch ${config} \
+  --selected_gpus="0,1,2,3,4,5,6,7" \
+  --log_dir=${LOGDIR} \
+  train.py --data_dir=./fast_resnet_data/ \
+  --num_epochs=30 \
+  --fp16=${FP16} \
+  --scale_loss=${SCALE_LOSS} \
+  --start_test_pass=26 \
+  --log_period=100 \
+  --fuse=True \
+  --profile=False 
+ 
