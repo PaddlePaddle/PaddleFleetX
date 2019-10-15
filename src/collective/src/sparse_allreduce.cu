50c8af407a3b8fc9c21c4b0091d213d0b79e17f4
WangXi <wangxi16@baidu.com>
2019-10-14 21:29:21 -0500

    Fix nan by stripping nccl from sparseReduce (#79)
    Former-commit-id: f5d02534e539eabaf65c5c27ad5d418d9007c80c
    Former-commit-id: 915350789e5b6faece8c08c90667bbb21bddd10b

--- src/collective/src/sparse_allreduce.cu
+++ src/collective/src/sparse_allreduce.cu
@@ -16,8 +16,7 @@ namespace dgc{
 #define MAX_BLOCK 32
 
 template<typename T>
-static void argsCheck(const void* encode, void* gatherbuff, const int nnz, T* dense, const int count) {
-  assert(encode != NULL);
+static void argsCheck(void* gatherbuff, const int nnz, T* dense, const int count) {
   assert(gatherbuff != NULL);
   assert(dense != NULL);
   assert(nnz < count);
@@ -39,34 +38,58 @@ void sparseReduceKernel(void* encode, const int nnz, T* dense, const int count, 
   }
 }
 
+#ifdef DEBUG_PRINT
+__global__
+void checkEncodeKernel(void* encode, const int nnz, const int count) {
+  __shared__ int flag;
+  const int tid = threadIdx.x + blockIdx.x * BLOCK_SIZE;
+  const int N = BLOCK_SIZE * MAX_BLOCK;
+
+  if (tid == 0) flag = 0;
+  __syncthreads();
+
+  int* index = static_cast<int*>(encode);
+  float* value = reinterpret_cast<float*>(index + nnz);
+  for (int j = tid; j < nnz; j += N) {
+    int idx = index[j];
+    if ( !((idx >= 0 && idx < count) || (idx == -1)) ) {
+      flag |= 4;
+      break;
+    }
+    if (idx == -1) continue;
+    if (isnan(value[j])) flag |= 1;
+    if (isinf(value[j])) flag |= 2;
+  }
+
+  if (tid == 0) {
+    if (flag & 1) printf("===== DGC Debug: gather has nan ====");
+    if (flag & 2) printf("===== DGC Debug: gather has inf ====");
+    if (flag & 4) printf("===== DGC Debug: gather idx ill ====");
+  }
+}
+#endif
+
 template<typename T>
-void sparseReduce(void* gatherbuff, const int nnz, T* dense, const int count,
-                  const int nranks, cudaStream_t stream) {
+void sparseReduceBuff(void* gatherbuff, const int nnz, T* dense, const int count,
+                      const int nranks, cudaStream_t stream) {
   const int blocks = min(DIVUP(nnz, BLOCK_SIZE), MAX_BLOCK);
   void* encode = gatherbuff;
   // Todo. cuda9 use cooperative groups for grid sync
   for (int i = 0; i < nranks; ++i) {
+#ifdef DEBUG_PRINT
+    checkEncodeKernel<<<blocks, BLOCK_SIZE, 0, stream>>>(encode, nnz, count);
+#endif
     sparseReduceKernel<<<blocks, BLOCK_SIZE, 0, stream>>>(encode, nnz, dense, count, nranks);
     encode = static_cast<void*>(static_cast<char*>(encode) + (sizeof(int)+sizeof(T)) * nnz);
   }
 }
 
 __attribute__ ((visibility("default")))
-bool sparseAllGReduce(const void* encode, void* gatherbuff, const int nnz, float* dense,
-                      const int count, ncclComm_t comm, cudaStream_t stream) {
-  argsCheck(encode, gatherbuff, nnz, dense, count);
+bool sparseReduce(void* gatherbuff, const int nnz, float* dense, const int count,
+                  const int nranks, cudaStream_t stream) {
+  argsCheck(gatherbuff, nnz, dense, count);
 
-  int device = -1;
-  warpNcclCommCuDevice(comm, &device);
-  assert(device != -1);
-  CudaDeviceGuard guard(device);
-
-  warpNcclAllGather(encode, gatherbuff, nnz * (sizeof(int) + sizeof(float)), ncclChar, comm, stream);
-
-  int nranks = 0;
-  warpNcclCommCount(comm, &nranks);
-  sparseReduce(gatherbuff, nnz, dense, count, nranks, stream);
-
+  sparseReduceBuff(gatherbuff, nnz, dense, count, nranks, stream);
   return true;
 }
 
