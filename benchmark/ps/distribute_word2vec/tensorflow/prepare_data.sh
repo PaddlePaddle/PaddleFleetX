6d52ca811a87b42e4ea22912643d4cffc3b2cb0b
123malin <malin10@baidu.com>
2019-10-21 21:21:55 +0800

    Word2vec fix (#82)
    * update word2vec
    
    * update word2vec tensorflow
    
    * fix word2vec tensorflow
    
    * fix word2vec tensorflow
    
    * update retry times
    
    * update train_epochs
    
    * update ctr_dnn tensorflow
    
    * update geo decay_steps

--- benchmark/ps/distribute_word2vec/tensorflow/prepare_data.sh
+++ benchmark/ps/distribute_word2vec/tensorflow/prepare_data.sh
@@ -10,15 +10,16 @@ mv 1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tok
 python preprocess.py --build_dict --build_dict_corpus_dir data/training-monolingual.tokenized.shuffled --dict_path data/test_build_dict
 python preprocess.py --filter_corpus --dict_path data/test_build_dict --input_corpus_dir data/training-monolingual.tokenized.shuffled --output_corpus_dir data/convert_text8 --min_count 5 --downsample 0.001
 mkdir thirdparty
-mv data/convert_text8 train_data
 mv data/test_build_dict thirdparty/
 mv data/test_build_dict_word_to_id_ thirdparty/
+
+python preprocess.py --data_resplit --input_corpus_dir=data/convert_text8 --output_corpus_dir=train_data
 
 # download test data
 wget https://paddlerec.bj.bcebos.com/word2vec/test_dir.tar
 tar xzvf test_dir.tar
 mv data/test_dir test_data/
-
+rm -rf data/
 
 
 
