4b4aea40db8e9a437ae6c57af0e256714c188b80
lilong12 <lilong12@baidu.com>
2019-10-22 20:26:23 +0800

    delete paddlecloud

--- examples/dist_classify/paddlecloud/end_hook.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ImageNet
-echo "PADDLE_TRAINERS:"
-echo ${PADDLE_TRAINERS}
-echo "POD_IP:"
-echo ${POD_IP}
-
-echo "end"
--- examples/dist_classify/paddlecloud/run.py
+++
@@ -1,6 +0,0 @@
-#!/bin/env python
-# -*- encoding:utf-8 -*-
-
-if __name__ == '__main__':
-    print("Done!")
-
