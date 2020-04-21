9f41f045e17bc5c2cae6485d99ecd7d5793e5794
tangwei <tangwei12@baidu.com>
2020-04-21 11:57:40 +0800

    add fleet-rec

---
+++ fleet-rec/fleetrec/core/trainers/cluster_trainer.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Training use fluid with one node only.
+"""
+
+from __future__ import print_function
+
+import paddle.fluid as fluid
+from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler import fleet
+from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler.distributed_strategy import StrategyFactory
+from paddle.fluid.incubate.fleet.base.role_maker import PaddleCloudRoleMaker
+
+from fleetrec.core.utils import envs
+from fleetrec.core.trainers.transpiler_trainer import TranspileTrainer
+
+
+class ClusterTrainer(TranspileTrainer):
+    def processor_register(self):
+        role = PaddleCloudRoleMaker()
+        fleet.init(role)
+
+        if fleet.is_server():
+            self.regist_context_processor('uninit', self.instance)
+            self.regist_context_processor('init_pass', self.init)
+            self.regist_context_processor('server_pass', self.server)
+        else:
+            self.regist_context_processor('uninit', self.instance)
+            self.regist_context_processor('init_pass', self.init)
+            self.regist_context_processor('train_pass', self.train)
+            self.regist_context_processor('terminal_pass', self.terminal)
+
+    def build_strategy(self):
+        mode = envs.get_runtime_environ("train.trainer.strategy")
+        assert mode in ["async", "geo", "sync", "half_async"]
+
+        strategy = None
+
+        if mode == "async":
+            strategy = StrategyFactory.create_async_strategy()
+        elif mode == "geo":
+            push_num = envs.get_global_env("train.strategy.mode.push_num", 100)
+            strategy = StrategyFactory.create_geo_strategy(push_num)
+        elif mode == "sync":
+            strategy = StrategyFactory.create_sync_strategy()
+        elif mode == "half_async":
+            strategy = StrategyFactory.create_half_async_strategy()
+
+        assert strategy is not None
+
+        return strategy
+
+    def init(self, context):
+        self.model.train_net()
+        optimizer = self.model.optimizer()
+        strategy = self.build_strategy()
+        optimizer = fleet.distributed_optimizer(optimizer, strategy)
+        optimizer.minimize(self.model.get_cost_op())
+
+        if fleet.is_server():
+            context['status'] = 'server_pass'
+        else:
+            self.fetch_vars = []
+            self.fetch_alias = []
+            self.fetch_period = self.model.get_fetch_period()
+
+            metrics = self.model.get_metrics()
+            if metrics:
+                self.fetch_vars = metrics.values()
+                self.fetch_alias = metrics.keys()
+            context['status'] = 'train_pass'
+
+    def server(self, context):
+        fleet.init_server()
+        fleet.run_server()
+        context['is_exit'] = True
+
+    def train(self, context):
+        self._exe.run(fleet.startup_program)
+        fleet.init_worker()
+
+        dataset = self._get_dataset()
+        epochs = envs.get_global_env("train.epochs")
+
+        for i in range(epochs):
+            self._exe.train_from_dataset(program=fluid.default_main_program(),
+                                         dataset=dataset,
+                                         fetch_list=self.fetch_vars,
+                                         fetch_info=self.fetch_alias,
+                                         print_period=self.fetch_period)
+            self.save(i, "train", is_fleet=True)
+        context['status'] = 'terminal_pass'
+        fleet.stop_worker()
+
+    def infer(self, context):
+        context['status'] = 'terminal_pass'
+
+    def terminal(self, context):
+        for model in self.increment_models:
+            print("epoch :{}, dir: {}".format(model[0], model[1]))
+        context['is_exit'] = True
---
+++ fleet-rec/fleetrec/run.py
@@ -0,0 +1,156 @@
+import argparse
+import os
+
+import yaml
+from paddle.fluid.incubate.fleet.parameter_server import version
+
+from fleetrec.core.factory import TrainerFactory
+from fleetrec.core.utils import envs
+from fleetrec.core.utils import util
+
+engines = {"TRAINSPILER": {}, "PSLIB": {}}
+clusters = ["SINGLE", "LOCAL_CLUSTER", "CLUSTER"]
+
+
+def set_runtime_envs(cluster_envs, engine_yaml):
+    def get_engine_extras():
+        with open(engine_yaml, 'r') as rb:
+            _envs = yaml.load(rb.read(), Loader=yaml.FullLoader)
+
+        flattens = envs.flatten_environs(_envs)
+
+        engine_extras = {}
+        for k, v in flattens.items():
+            if k.startswith("train.trainer."):
+                engine_extras[k] = v
+        return engine_extras
+
+    if cluster_envs is None:
+        cluster_envs = {}
+
+    envs.set_runtime_environs(cluster_envs)
+    envs.set_runtime_environs(get_engine_extras())
+
+    need_print = {}
+    for k, v in os.environ.items():
+        if k.startswith("train.trainer."):
+            need_print[k] = v
+
+    print(envs.pretty_print_envs(need_print, ("Runtime Envs", "Value")))
+
+
+def get_engine(engine):
+    engine = engine.upper()
+    if version.is_transpiler():
+        run_engine = engines["TRAINSPILER"].get(engine, None)
+    else:
+        run_engine = engines["PSLIB"].get(engine, None)
+
+    if run_engine is None:
+        raise ValueError("engine only support SINGLE/LOCAL_CLUSTER/CLUSTER")
+    return run_engine
+
+
+def single_engine(args):
+    print("use single engine to run model: {}".format(args.model))
+
+    single_envs = {}
+    single_envs["train.trainer.trainer"] = "SingleTrainer"
+    single_envs["train.trainer.threads"] = "2"
+    single_envs["train.trainer.engine"] = "single"
+    set_runtime_envs(single_envs, args.model)
+    trainer = TrainerFactory.create(args.model)
+    return trainer
+
+
+def cluster_engine(args):
+    print("launch cluster engine with cluster to run model: {}".format(args.model))
+
+    cluster_envs = {}
+    cluster_envs["train.trainer.trainer"] = "ClusterTrainer"
+    cluster_envs["train.trainer.engine"] = "cluster"
+    set_runtime_envs(cluster_envs, args.model)
+
+    trainer = TrainerFactory.create(args.model)
+    return trainer
+
+
+def cluster_mpi_engine(args):
+    print("launch cluster engine with cluster to run model: {}".format(args.model))
+
+    cluster_envs = {}
+    cluster_envs["train.trainer.trainer"] = "CtrCodingTrainer"
+    set_runtime_envs(cluster_envs, args.model)
+
+    trainer = TrainerFactory.create(args.model)
+    return trainer
+
+
+def local_cluster_engine(args):
+    print("launch cluster engine with cluster to run model: {}".format(args.model))
+    from fleetrec.core.engine.local_cluster_engine import LocalClusterEngine
+
+    cluster_envs = {}
+    cluster_envs["server_num"] = 1
+    cluster_envs["worker_num"] = 1
+    cluster_envs["start_port"] = 36001
+    cluster_envs["log_dir"] = "logs"
+    cluster_envs["train.trainer.trainer"] = "ClusterTrainer"
+    cluster_envs["train.trainer.strategy"] = "async"
+    cluster_envs["train.trainer.threads"] = "2"
+    cluster_envs["train.trainer.engine"] = "local_cluster"
+    cluster_envs["CPU_NUM"] = "2"
+
+    set_runtime_envs(cluster_envs, args.model)
+
+    launch = LocalClusterEngine(cluster_envs, args.model)
+    return launch
+
+
+def local_mpi_engine(args):
+    print("launch cluster engine with cluster to run model: {}".format(args.model))
+    from fleetrec.core.engine.local_mpi_engine import LocalMPIEngine
+
+    print("use 1X1 MPI ClusterTraining at localhost to run model: {}".format(args.model))
+
+    mpi = util.run_which("mpirun")
+    if not mpi:
+        raise RuntimeError("can not find mpirun, please check environment")
+    cluster_envs = {}
+    cluster_envs["mpirun"] = mpi
+    cluster_envs["train.trainer.trainer"] = "CtrCodingTrainer"
+    cluster_envs["log_dir"] = "logs"
+    cluster_envs["train.trainer.engine"] = "local_cluster"
+
+    set_runtime_envs(cluster_envs, args.model)
+    launch = LocalMPIEngine(cluster_envs, args.model)
+    return launch
+
+
+def engine_registry():
+    engines["TRAINSPILER"]["SINGLE"] = single_engine
+    engines["TRAINSPILER"]["LOCAL_CLUSTER"] = local_cluster_engine
+    engines["TRAINSPILER"]["CLUSTER"] = cluster_engine
+    engines["PSLIB"]["SINGLE"] = local_mpi_engine
+    engines["PSLIB"]["LOCAL_CLUSTER"] = local_mpi_engine
+    engines["PSLIB"]["CLUSTER"] = cluster_mpi_engine
+
+
+engine_registry()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='fleet-rec run')
+    parser.add_argument("-m", "--model", type=str)
+    parser.add_argument("-e", "--engine", type=str)
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.model) or not os.path.isfile(args.model):
+        raise ValueError("argument model: {} error, must specify an existed YAML file".format(args.model))
+
+    if args.engine.upper() not in clusters:
+        raise ValueError("argument engine: {} error, must in {}".format(args.engine, clusters))
+
+    which_engine = get_engine(args.engine)
+    engine = which_engine(args)
+    engine.run()
