0bf7cb56d257f975f22dc9d862a43ac492dd4d2c
barrierye <barriery@qq.com>
2020-08-12 01:57:35 +0000

    update readme

--- examples/dam/dist_run.sh
+++ examples/dam/dist_run.sh
@@ -15,12 +15,13 @@ python -m paddle.distributed.launch \
              --do_train True \
              --train_data_path data/ubuntu/train.txt \
              --valid_data_path data/ubuntu/valid.txt \
+             --word_emb_init data/ubuntu/word_embedding.pkl \
              --vocab_path data/ubuntu/word2id \
              --data_source ubuntu \
              --save_path ./model_files/ubuntu \
              --vocab_size 434512 \
-             --batch_size 256 \
-             --num_scan_data 5
+             --batch_size 32 \
+             --num_scan_data 2
 
 # douban dist train
 python -m paddle.distributed.launch \
@@ -30,11 +31,12 @@ python -m paddle.distributed.launch \
             --do_train True \
             --train_data_path data/douban/train.txt \
             --valid_data_path data/douban/dev.txt \
+            --word_emb_init data/douban/word_embedding.pkl \
             --vocab_path data/douban/word2id \
             --data_source douban \
             --save_path ./model_files/douban \
             --vocab_size 172130 \
             --channel1_num 16 \
-            --batch_size 256 \
-            --num_scan_data 5 \
+            --batch_size 32 \
+            --num_scan_data 2 \
             --ext_eval
--- examples/dam/run.sh
+++ examples/dam/run.sh
@@ -4,13 +4,14 @@ export FLAGS_eager_delete_tensor_gb=0.0
 # train on ubuntu
 python -u train.py \
   --do_train True \
-  --train_data_path data_small/ubuntu/train.txt \
-  --valid_data_path data_small/ubuntu/valid.txt \
+  --train_data_path data/ubuntu/train.txt \
+  --valid_data_path data/ubuntu/valid.txt \
+  --word_emb_init data/ubuntu/word_embedding.pkl \
   --vocab_path data/ubuntu/word2id \
   --data_source ubuntu \
   --save_path ./model_files/ubuntu \
   --vocab_size 434512 \
-  --batch_size 16 \
+  --batch_size 1 \
   --num_scan_data 1
 
 #test on ubuntu
@@ -19,7 +20,7 @@ python -u train.py \
   --test_data_path data/ubuntu/test.txt \
   --vocab_path data/ubuntu/word2id \
   --data_source ubuntu \
-  --model_path model_files/ubuntu/model.4 \
+  --model_path ./model_files/ubuntu/model.epoch_0.step_6000 \
   --save_path model_files/ubuntu/result \
   --vocab_size 434512 \
   --batch_size 512
@@ -29,6 +30,7 @@ python -u train.py
   --do_train True \
   --train_data_path data/douban/train.txt \
   --valid_data_path data/douban/dev.txt \
+  --word_emb_init data/douban/word_embedding.pkl \
   --vocab_path data/douban/word2id \
   --data_source douban \
   --save_path ./model_files/douban \
@@ -41,10 +43,10 @@ python -u train.py
 python -u train.py \
   --do_test True \
   --ext_eval \
-  --test_data_path data/douban/dev.txt \
+  --test_data_path data/douban/test.txt \
   --vocab_path data/douban/word2id \
   --data_source douban \
-  --model_path model_files/douban/model.4 \
+  --model_path ./model_files/douban/model.epoch_0.step_6000 \
   --save_path model_files/douban/result \
   --vocab_size 172130 \
   --channel1_num 16 \
