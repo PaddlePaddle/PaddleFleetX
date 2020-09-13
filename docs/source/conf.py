88b52ef323090685876500685e9e03c55bae37fb
dongdaxiang <dongdaxiang@baidu.com>
2020-09-13 21:55:48 +0800

    update format for conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ conf.py
 """
 import os
 import sys
-sys.path.append(os.path.abspath('../../Fleet/'))
+sys.path.append(os.path.abspath('../../FleetX/'))
 sys.path.append(os.path.abspath('../..'))
 sys.path.append(os.path.abspath('..'))
 import sphinx_rtd_theme
@@ -82,7 +82,25 @@ html_static_path = ['_static']
 
 # for latex bug
 #latex_engine = 'xelatex'
-latex_engine = 'pdflatex'
+#latex_engine = 'pdflatex'
+latex_engine = "xelatex"
+
+latex_elements={# The paper size ('letterpaper' or 'a4paper').
+    'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
+    'pointsize':'12pt','classoptions':',oneside','babel':'',
+    'inputenc':'',
+    'utf8extra':'',
+    'preamble': r"""
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+"""}
 
 extensions = ['recommonmark']
 
