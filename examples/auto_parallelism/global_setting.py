404731c3a97c78e0cd2b7199715e9c0a5b20a4a1
caozhou <caozhou@radi.ac.cn>
2021-10-07 15:15:11 +0000

    add auto parallel gpt demo

---
+++ examples/auto_parallelism/global_setting.py
@@ -0,0 +1,8 @@
+def init_global():
+    global _global_parallel_stratergy
+    global _global_process_mesh
+    global ROOT_MESH
+    global PP_MESH_LIST
+    global DPPP_MESH_LIST
+    global MPPP_MESH_LIST
+    global DPMPPP_MESH_LIST
