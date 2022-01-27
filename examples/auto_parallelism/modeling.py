68dfa21256890f8d13a003c6f13d95943a61affb
juneweng <hopewj@163.com>
2022-01-27 11:12:05 +0800

    add auto search ce

--- examples/auto_parallelism/modeling.py
+++ examples/auto_parallelism/modeling.py
@@ -920,27 +920,4 @@ class GPTPretrainingCriterion(nn.Layer):
         loss_mask = loss_mask.reshape([-1])
         masked_lm_loss = paddle.sum(masked_lm_loss.reshape([-1]) * loss_mask)
         total_loss = masked_lm_loss / loss_mask.sum()
-        pp_total_loss = None
-        if self.topo.pp_info.size > 1:
-            total_loss = total_loss
-            masked_lm_loss.persistable = True
-            total_loss.persistable = True
-            total_loss.persistable = True
-            pp_total_loss = paddle.fluid.layers.fill_constant([1, ], "float32", 0.0)
-            pp_total_loss.persistable = True
-            block = paddle.static.default_main_program().global_block()
-            acc_steps = self.args.global_batch_size // self.topo.data_info.size // self.args.micro_batch_size
-            tmp = total_loss / acc_steps
-            block.append_op(
-                type="elementwise_add",
-                inputs={
-                    "X": [pp_total_loss],
-                    "Y": [tmp]
-                },
-                outputs={"Out": [pp_total_loss]}
-            )
-        loss_vars = {
-            "total_loss": total_loss,
-            "pp_total_loss": pp_total_loss
-        }
-        return loss_vars
+        return total_loss
