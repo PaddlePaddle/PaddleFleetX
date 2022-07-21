659093edd46b5a5e138e29905a418bd6649d8090
ForFishes <2282912238@qq.com>
2022-07-21 06:55:12 +0000

    add single training

---
+++ examples/gpt/single/run.sh
@@ -0,0 +1,36 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export PYTHONPATH=$PYTHONPATH:../../../
+
+python run_pretrain.py \
+    --input_dir "./data"\
+    --output_dir "output"\
+    --vocab_size 50304\
+    --hidden_size 1024\
+    --num_layers 16\
+    --num_attention_heads 8\
+    --max_seq_len 1024\
+    --weight_decay 0.01\
+    --grad_clip 1.0\
+    --max_steps 500000\
+    --save_steps 100000\
+    --decay_steps 320000\
+    --device gpu\
+    --eval_freq 1000\
+    --warmup_rate 0.01\
+    --scale_loss 32768\
+    --global_batch_size 16\
+    --micro_batch_size 8\
+    --use_pure_fp16 True
