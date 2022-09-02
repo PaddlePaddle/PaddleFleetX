5f50589b07b077fc79fdcce2a27dd25b9edecee1
zhaoyingli <zhaoyingli@baidu.com>
2022-09-02 11:12:42 +0800

    fix autoengine

--- examples/gpt/auto_parallel/run_pretrain.py
+++ examples/gpt/auto_parallel/run_pretrain.py
@@ -17,11 +17,13 @@ import sys
 import numpy as np
 
 import paddle
+from paddle.static import InputSpec
 from paddle.distributed import fleet
 
 sys.path.append("../../../")
 from examples.gpt.tools import parse_yaml_auto
 from examples.gpt.gpt_module import GPTAutoModule
+from fleetx.data.sampler import Stack, Tuple
 from fleetx.core.engine import AutoEngine
 from fleetx.data.tokenizers import GPTTokenizer
 from fleetx.datasets.gpt import create_pretrained_dataset_auto, get_train_data_file
@@ -40,7 +42,26 @@ def generate_dist_strategy(configs):
         "init_loss_scaling": amp_configs['scale_loss'],
         "use_pure_fp16": amp_configs['use_pure_fp16'],
     }
+    sharding_configs = configs['Distributed']['sharding']
+    dist_strategy.sharding = sharding_configs['sharding_degree'] > 1
+    dist_strategy.sharding_configs = {
+        "sharding_degree": sharding_configs['sharding_degree'],
+        "stage": sharding_configs['sharding_stage']
+    }
     return dist_strategy
+
+
+def generate_data_holder(configs):
+    data_configs = configs['Data']
+    gbsz = data_configs['batch_size']['global_batch_size']
+    max_seq_len = data_configs['dataset']['max_seq_len']
+
+    tokens = InputSpec([gbsz, max_seq_len], "int64", "tokens")
+    position_ids = InputSpec([gbsz, max_seq_len], "int64", "position_ids")
+    labels = InputSpec([gbsz, max_seq_len], "int64", "labels")
+    loss_mask = InputSpec([gbsz, max_seq_len], "float32", "loss_mask")
+
+    return [tokens, position_ids], [labels, loss_mask]
 
 
 def do_train():
@@ -52,12 +73,19 @@ def do_train():
 
     fleet.init(is_collective=True)
     dist_strategy = generate_dist_strategy(configs)
-    tokenizer = GPTTokenizer.from_pretrained("gpt2")
+    inputs_spec, labels_spec = generate_data_holder(configs)
 
     module = GPTAutoModule(configs)
-    engine = AutoEngine(module=module, configs=configs, strategy=dist_strategy)
+    engine = AutoEngine(
+        module=module,
+        configs=configs,
+        inputs_spec=inputs_spec,
+        labels_spec=labels_spec,
+        strategy=dist_strategy)
     engine.load()
 
+    gbsz = configs['Data']['batch_size']['global_batch_size']
+    tokenizer = GPTTokenizer.from_pretrained("gpt2")
     for epoch in range(configs['Engine']['num_train_epochs']):
         files = get_train_data_file(configs['Data']['dataset']['input_dir'])
         files.sort()
@@ -66,11 +94,12 @@ def do_train():
             data_file = files[f_id]
             train_data, valid_data, test_data = create_pretrained_dataset_auto(
                 configs, [data_file], tokenizer.eos_token_id)
-            engine.fit(train_dataset=train_data, epoch=epoch)
+            engine.fit(train_dataset=train_data,
+                       batch_size=gbsz,
+                       collate_fn=Tuple(Stack(), Stack(), Stack(), Stack()))
 
-            # engine.fit(train_dataset=train_data, valid_dataset=valid_data, epoch=epoch)
-            # engine.evaluate(valid_dataset=valid_data)
-            # engine.predict(test_dataset=test_data)
+            # engine.evaluate(valid_dataset=valid_data, batch_size=gbsz, collate_fn=Tuple(Stack(), Stack(), Stack(), Stack()))
+            # engine.predict(test_dataset=test_data, batch_size=gbsz, collate_fn=Tuple(Stack(), Stack(), Stack(), Stack()))
             # engine.save()
 
 
