ca694d99ac2b1a1bb0d60150d84567a8c688db17
ForFishes <2282912238@qq.com>
2022-09-19 11:04:21 +0000

    fix bug of infer

--- tasks/gpt/inference.py
+++ tasks/gpt/inference.py
@@ -25,7 +25,7 @@ import paddle.distributed as dist
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.abspath(os.path.join(__dir__, '../../')))
 
-from ppfleetx.utils import config, env, logger
+from ppfleetx.utils import config, env
 from ppfleetx.utils.log import logger
 from ppfleetx.data import build_dataloader, tokenizers
 from ppfleetx.models import build_module
