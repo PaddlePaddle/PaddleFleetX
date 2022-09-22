64ec5186206af6d120a13fcc9339dac95c461ee0
haohongxiang <haohongxiang@baidu.com>
2022-09-22 15:50:23 +0000

    fix train.py

--- tools/train.py
+++ tools/train.py
@@ -59,9 +59,6 @@ if __name__ == "__main__":
     if cfg.Engine.save_load.ckpt_dir is not None:
         engine.load()
 
-    train_data_loader = build_dataloader(cfg.Data, "Train")
-    eval_data_loader = build_dataloader(cfg.Data, "Eval")
-
     engine.fit(train_data_loader=train_data_loader,
                valid_data_loader=eval_data_loader,
                epoch=cfg.Engine.num_train_epochs)
