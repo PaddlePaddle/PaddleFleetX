8da2139361d66b73fa7cd4ecebef5917b4e78c05
GuoxiaWang <mingzilaochongtu@gmail.com>
2022-10-10 11:00:52 +0800

    add distributed communication and layers for helixfold

---
+++ ppfleetx/models/protein_folding/common.py
@@ -0,0 +1,247 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import paddle
+import paddle.nn as nn
+
+try:
+    from paddle import _legacy_C_ops as _C_ops
+except:
+    from paddle import _C_ops
+
+
+class OuterProductMean(nn.Layer):
+    """Computes mean outer product.
+
+    Jumper et al. (2021) Suppl. Alg. 10 "OuterProductMean"
+    """
+
+    def __init__(self,
+                 channel_num,
+                 config,
+                 global_config,
+                 is_extra_msa,
+                 name='outer_product_mean'):
+        super(OuterProductMean, self).__init__()
+        self.channel_num = channel_num
+        self.config = config
+        self.global_config = global_config
+
+        if is_extra_msa:
+            c_m = channel_num['extra_msa_channel']
+        else:
+            c_m = channel_num['msa_channel']
+
+        self.layer_norm_input = nn.LayerNorm(c_m, name='layer_norm_input')
+        self.left_projection = nn.Linear(
+            c_m, self.config.num_outer_channel, name='left_projection')
+        self.right_projection = nn.Linear(
+            c_m, self.config.num_outer_channel, name='right_projection')
+
+        if self.global_config.zero_init:
+            init_w = nn.initializer.Constant(value=0.0)
+        else:
+            init_w = nn.initializer.KaimingNormal()
+
+        self.output_w = paddle.create_parameter(
+            [
+                self.config.num_outer_channel, self.config.num_outer_channel,
+                channel_num['pair_channel']
+            ],
+            'float32',
+            default_initializer=init_w)
+        self.output_b = paddle.create_parameter(
+            [channel_num['pair_channel']],
+            'float32',
+            default_initializer=nn.initializer.Constant(value=0.0))
+
+    def forward(self, act, mask):
+        """Builds OuterProductMean module.
+
+        Arguments:
+        act: MSA representation, shape [batch, N_seq, N_res, c_m].
+        mask: MSA mask, shape [batch, N_seq, N_res].
+
+        Returns:
+        Update to pair representation, shape [batch, N_res, N_res, c_z].
+        """
+        # [B, N_seq, N_res//dap_size, c_m]
+        act = self.layer_norm_input(act)
+        # [B, N_seq, N_res//dap_size, c_m] => [B, N_seq, N_res//dap_size, num_outer_channel]
+        right_act_before = self.right_projection(act)
+        # [B, N_seq, N_res//dap_size, num_outer_channel] => [B, N_seq, N_res, num_outer_channel]
+        right_act = dap.all_gather(right_act_before, axis=2)
+
+        # [B, N_seq, N_res//dap_size, c_m] => [B, N_seq, N_res//dap_size, num_outer_channel]
+        left_act = self.left_projection(act)
+        # [B, N_seq, N_res] => [B, N_seq, N_res, 1]
+        mask = paddle.unsqueeze(mask, axis=-1)
+        # [B, N_seq, N_res, 1] => [B, N_seq, N_res//dap_size, 1]
+        mask_col = dap.scatter(mask, axis=2)
+        left_act = mask_col * left_act
+
+        # [B, N_seq, N_res//dap_size, 1], [B, N_seq, N_res, 1] => [B, N_res//dap_size, N_res, 1]
+        epsilon = 1e-3
+        norm = paddle.einsum('nabc,nadc->nbdc', mask_col, mask) + epsilon
+
+        def compute_chunk(left_act, right_act):
+            # This is equivalent to
+            #
+            # act = jnp.einsum('abc,ade->dceb', left_act, right_act)
+            # act = jnp.einsum('dceb,cef->bdf', act, output_w) + output_b
+            #
+            # but faster. maybe for subbatch inference?
+
+            # [B, N_seq, N_res//dap_size, num_outer_channel] => [B, N_seq, num_outer_channel, N_res//dap_size]
+            left_act = left_act.transpose([0, 1, 3, 2])
+            # wait if using async communication and dap, otherwise do nothing
+            right_act_after = dap.all_gather_opp(right_act, axis=2)
+            # [B, N_seq, num_outer_channel, N_res//dap_size], [B, N_seq, N_res, num_outer_channel]
+            # => [B, N_res, num_outer_channel, num_outer_channel, N_res//dap_size]
+            act = paddle.einsum('nacb,nade->ndceb', left_act, right_act_after)
+            # [B, N_res, num_outer_channel, num_outer_channel, N_res//dap_size], [num_outer_channel, num_outer_channel, c_z]
+            # => [B, N_res, N_res//dap_size, c_z]
+            act = paddle.einsum('ndceb,cef->ndbf', act,
+                                self.output_w) + self.output_b
+            # [B, N_res, N_res//dap_size, c_z] => [B, N_res//dap_size, N_res, c_z]
+            return act.transpose([0, 2, 1, 3])
+
+        if not self.training:
+            # low memory mode using subbatch
+            sb_chunk = subbatch(compute_chunk, [0], [2],
+                                self.config.chunk_size, 1)
+            act = sb_chunk(left_act, right_act)
+        else:
+            act = compute_chunk(left_act, right_act)
+
+        act = act / norm
+
+        return act
+
+
+class Transition(nn.Layer):
+    """Transition layer.
+
+    Jumper et al. (2021) Suppl. Alg. 9 "MSATransition"
+    Jumper et al. (2021) Suppl. Alg. 15 "PairTransition"
+    """
+
+    def __init__(self, channel_num, config, global_config, is_extra_msa,
+                 transition_type):
+        super(Transition, self).__init__()
+        assert transition_type in ['msa_transition', 'pair_transition']
+        self.channel_num = channel_num
+        self.config = config
+        self.global_config = global_config
+        self.is_extra_msa = is_extra_msa
+        self.transition_type = transition_type
+
+        if transition_type == 'msa_transition' and is_extra_msa:
+            in_dim = channel_num['extra_msa_channel']
+        elif transition_type == 'msa_transition' and not is_extra_msa:
+            in_dim = channel_num['msa_channel']
+        elif transition_type == 'pair_transition':
+            in_dim = channel_num['pair_channel']
+
+        self.input_layer_norm = nn.LayerNorm(in_dim)
+        self.transition1 = nn.Linear(
+            in_dim,
+            int(in_dim * self.config.num_intermediate_factor),
+            weight_attr=paddle.ParamAttr(
+                initializer=nn.initializer.KaimingNormal()))
+
+        if self.global_config.zero_init:
+            last_init = nn.initializer.Constant(0.0)
+        else:
+            last_init = nn.initializer.TruncatedNormal()
+
+        self.transition2 = nn.Linear(
+            int(in_dim * self.config.num_intermediate_factor),
+            in_dim,
+            weight_attr=paddle.ParamAttr(initializer=last_init))
+
+    def forward(self, act, mask):
+        act = self.input_layer_norm(act)
+
+        def transition_module(x):
+            x = self.transition1(x)
+            x = nn.functional.relu(x)
+            x = self.transition2(x)
+            return x
+
+        if not self.training:
+            # low memory mode using subbatch
+            sb_transition = subbatch(transition_module, [0], [1],
+                                     self.global_config.subbatch_size, 1)
+            act = sb_transition(act)
+        else:
+            act = transition_module(act)
+
+        return act
+
+
+class Dropout(nn.Layer):
+    def __init__(self, p=0.5, axis=None, mode="upscale_in_train", name=None):
+        super(Dropout, self).__init__()
+
+        if not isinstance(p, (float, int)):
+            raise TypeError("p argument should be a number")
+        if p < 0 or p > 1:
+            raise ValueError("p argument should between 0 and 1")
+
+        mode = 'downgrade_in_infer' if mode == 'downscale_in_infer' else mode  #semantic transfer
+        if mode not in ('downscale_in_infer', 'upscale_in_train'):
+            raise ValueError(
+                "mode argument should be 'downscale_in_infer' or 'upscale_in_train'"
+            )
+
+        if axis and not isinstance(axis, (int, list, tuple)):
+            raise TypeError("datatype of axis argument should be int or list")
+
+        self.p = p
+        self.axis = axis
+        self.mode = mode
+        self.name = name
+
+    def forward(self, input):
+        # fast return for p == 0
+        if self.p == 0:
+            return input
+
+        if self.axis == None:
+            out = nn.functional.dropout(
+                input,
+                p=self.p,
+                axis=self.axis,
+                training=self.training,
+                mode=self.mode,
+                name=self.name)
+        else:
+            seed = None
+            drop_axes = [self.axis] if isinstance(self.axis,
+                                                  int) else list(self.axis)
+            if paddle.static.default_main_program().random_seed != 0:
+                seed = paddle.static.default_main_program().random_seed
+
+            out, mask = _C_ops.dropout_nd(
+                input, 'dropout_prob', self.p, 'is_test', not self.training,
+                'fix_seed', seed is not None, 'seed', seed if seed is not None
+                else 0, 'dropout_implementation', self.mode, 'axis', drop_axes)
+
+        return out
+
+    def extra_repr(self):
+        name_str = ', name={}'.format(self.name) if self.name else ''
+        return 'p={}, axis={}, mode={}{}'.format(self.p, self.axis, self.mode,
+                                                 name_str)
---
+++ ppfleetx/models/protein_folding/template.py
@@ -0,0 +1,352 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import paddle
+import paddle.nn as nn
+
+from ppfleetx.distributed.protein_folding import dap
+
+from .attentions import (
+    TriangleMultiplication,
+    TriangleAttention, )
+
+from .common import (
+    Transition,
+    Dropout, )
+
+
+class TemplatePair(nn.Layer):
+    """Pair processing for the templates.
+
+    Jumper et al. (2021) Suppl. Alg. 16 "TemplatePairStack" lines 2-6
+    """
+
+    def __init__(self, channel_num, config, global_config):
+        super(TemplatePair, self).__init__()
+        self.config = config
+        self.global_config = global_config
+
+        channel_num = {}
+        channel_num[
+            'pair_channel'] = self.config.triangle_attention_ending_node.value_dim
+
+        self.triangle_attention_starting_node = TriangleAttention(
+            channel_num,
+            self.config.triangle_attention_starting_node,
+            self.global_config,
+            name='triangle_attention_starting_node')
+
+        dropout_rate, dropout_axis = self._parse_dropout_params(
+            self.triangle_attention_starting_node)
+        self.triangle_starting_dropout = nn.Dropout(dropout_rate, axis=dropout_axis) \
+            if not self.global_config.use_dropout_nd else Dropout(dropout_rate, axis=dropout_axis)
+
+        self.triangle_attention_ending_node = TriangleAttention(
+            channel_num,
+            self.config.triangle_attention_ending_node,
+            self.global_config,
+            name='triangle_attention_ending_node')
+
+        dropout_rate, dropout_axis = self._parse_dropout_params(
+            self.triangle_attention_ending_node)
+        self.triangle_ending_dropout = nn.Dropout(dropout_rate, axis=dropout_axis) \
+            if not self.global_config.use_dropout_nd else Dropout(dropout_rate, axis=dropout_axis)
+
+        self.triangle_multiplication_outgoing = TriangleMultiplication(
+            channel_num,
+            self.config.triangle_multiplication_outgoing,
+            self.global_config,
+            name='triangle_multiplication_outgoing')
+
+        dropout_rate, dropout_axis = self._parse_dropout_params(
+            self.triangle_multiplication_outgoing)
+        self.triangle_outgoing_dropout = nn.Dropout(dropout_rate, axis=dropout_axis) \
+            if not self.global_config.use_dropout_nd else Dropout(dropout_rate, axis=dropout_axis)
+
+        self.triangle_multiplication_incoming = TriangleMultiplication(
+            channel_num,
+            self.config.triangle_multiplication_incoming,
+            self.global_config,
+            name='triangle_multiplication_incoming')
+
+        dropout_rate, dropout_axis = self._parse_dropout_params(
+            self.triangle_multiplication_incoming)
+        self.triangle_incoming_dropout = nn.Dropout(dropout_rate, axis=dropout_axis) \
+            if not self.global_config.use_dropout_nd else Dropout(dropout_rate, axis=dropout_axis)
+
+        self.pair_transition = Transition(
+            channel_num,
+            self.config.pair_transition,
+            self.global_config,
+            is_extra_msa=False,
+            transition_type='pair_transition')
+
+        dropout_rate, dropout_axis = self._parse_dropout_params(
+            self.pair_transition)
+        self.pair_transition_dropout = nn.Dropout(dropout_rate, axis=dropout_axis) \
+            if not self.global_config.use_dropout_nd else Dropout(dropout_rate, axis=dropout_axis)
+
+    def _parse_dropout_params(self, module):
+        dropout_rate = 0.0 if self.global_config.deterministic else \
+            module.config.dropout_rate
+        dropout_axis = None
+        if module.config.shared_dropout:
+            dropout_axis = {
+                'per_row': [0, 2, 3],
+                'per_column': [0, 1, 3],
+            }[module.config.orientation]
+
+        return dropout_rate, dropout_axis
+
+    def forward(self, pair_act, pair_mask):
+        """Builds one block of TemplatePair module.
+
+        Arguments:
+        pair_act: Pair activations for single template, shape [batch, N_res, N_res, c_t].
+        pair_mask: Pair mask, shape [batch, N_res, N_res].
+
+        Returns:
+        Updated pair_act, shape [batch, N_res, N_res, c_t].
+        """
+
+        pair_mask_row = dap.scatter(pair_mask, axis=1)
+        pair_mask_col = dap.scatter(pair_mask, axis=2)
+
+        residual = self.triangle_attention_starting_node(pair_act,
+                                                         pair_mask_row)
+        residual = self.triangle_starting_dropout(residual)
+        pair_act = pair_act + residual
+
+        pair_act = dap.row_to_col(pair_act)
+        residual = self.triangle_attention_ending_node(pair_act, pair_mask_col)
+        residual = self.triangle_ending_dropout(residual)
+        pair_act = pair_act + residual
+
+        pair_act = dap.col_to_row(pair_act)
+        residual = self.triangle_multiplication_outgoing(pair_act,
+                                                         pair_mask_row)
+        residual = self.triangle_outgoing_dropout(residual)
+        pair_act = pair_act + residual
+
+        pair_act = dap.row_to_col(pair_act)
+        residual = self.triangle_multiplication_incoming(pair_act,
+                                                         pair_mask_col)
+        residual = self.triangle_incoming_dropout(residual)
+        pair_act = pair_act + residual
+
+        residual = self.pair_transition(pair_act, pair_mask)
+        residual = self.pair_transition_dropout(residual)
+        pair_act = pair_act + residual
+
+        pair_act = dap.col_to_row(pair_act)
+
+        return pair_act
+
+
+class SingleTemplateEmbedding(nn.Layer):
+    """Embeds a single template.
+
+    Jumper et al. (2021) Suppl. Alg. 2 "Inference" lines 9+11
+    """
+
+    def __init__(self, channel_num, config, global_config):
+        super(SingleTemplateEmbedding, self).__init__()
+        self.config = config
+        self.channel_num = channel_num
+        self.global_config = global_config
+
+        self.embedding2d = nn.Linear(channel_num['template_pair'],
+                                     self.config.template_pair_stack.
+                                     triangle_attention_ending_node.value_dim)
+
+        self.template_pair_stack = nn.LayerList()
+        for _ in range(self.config.template_pair_stack.num_block):
+            self.template_pair_stack.append(
+                TemplatePair(self.channel_num, self.config.template_pair_stack,
+                             self.global_config))
+
+        self.output_layer_norm = nn.LayerNorm(self.config.attention.key_dim)
+
+    def forward(self, query_embedding, batch, mask_2d):
+        """Build the single template embedding.
+
+        Arguments:
+            query_embedding: Query pair representation, shape [batch, N_res, N_res, c_z].
+            batch: A batch of template features (note the template dimension has been
+                stripped out as this module only runs over a single template).
+            mask_2d: Padding mask (Note: this doesn't care if a template exists,
+                unlike the template_pseudo_beta_mask).
+
+        Returns:
+            A template embedding [N_res, N_res, c_z].
+        """
+        assert mask_2d.dtype == query_embedding.dtype
+        dtype = query_embedding.dtype
+        num_res = batch['template_aatype'].shape[1]
+        template_mask = batch['template_pseudo_beta_mask']
+        template_mask_2d = template_mask[..., None] * template_mask[...,
+                                                                    None, :]
+        template_mask_2d = template_mask_2d.astype(dtype)
+
+        template_dgram = dgram_from_positions(batch['template_pseudo_beta'],
+                                              **self.config.dgram_features)
+        template_dgram = template_dgram.astype(dtype)
+
+        aatype = nn.functional.one_hot(batch['template_aatype'], 22)
+        aatype = aatype.astype(dtype)
+
+        to_concat = [template_dgram, template_mask_2d[..., None]]
+        to_concat.append(
+            paddle.tile(aatype[..., None, :, :], [1, num_res, 1, 1]))
+        to_concat.append(paddle.tile(aatype[..., None, :], [1, 1, num_res, 1]))
+
+        n, ca, c = [residue_constants.atom_order[a] for a in ('N', 'CA', 'C')]
+        rot, trans = quat_affine.make_transform_from_reference(
+            n_xyz=batch['template_all_atom_positions'][..., n, :],
+            ca_xyz=batch['template_all_atom_positions'][..., ca, :],
+            c_xyz=batch['template_all_atom_positions'][..., c, :])
+        affines = quat_affine.QuatAffine(
+            quaternion=quat_affine.rot_to_quat(rot),
+            translation=trans,
+            rotation=rot)
+
+        points = [
+            paddle.unsqueeze(
+                x, axis=-2) for x in paddle.unstack(
+                    affines.translation, axis=-1)
+        ]
+        affine_vec = affines.invert_point(points, extra_dims=1)
+        inv_distance_scalar = paddle.rsqrt(1e-6 + sum(
+            [paddle.square(x) for x in affine_vec]))
+
+        # Backbone affine mask: whether the residue has C, CA, N
+        # (the template mask defined above only considers pseudo CB).
+        template_mask = (batch['template_all_atom_masks'][..., n] *
+                         batch['template_all_atom_masks'][..., ca] *
+                         batch['template_all_atom_masks'][..., c])
+        template_mask_2d = template_mask[..., None] * template_mask[...,
+                                                                    None, :]
+        inv_distance_scalar *= template_mask_2d.astype(
+            inv_distance_scalar.dtype)
+
+        unit_vector = [(x * inv_distance_scalar)[..., None]
+                       for x in affine_vec]
+        unit_vector = [x.astype(dtype) for x in unit_vector]
+        if not self.config.use_template_unit_vector:
+            unit_vector = [paddle.zeros_like(x) for x in unit_vector]
+        to_concat.extend(unit_vector)
+
+        template_mask_2d = template_mask_2d.astype(dtype)
+        to_concat.append(template_mask_2d[..., None])
+
+        act = paddle.concat(to_concat, axis=-1)
+        # Mask out non-template regions so we don't get arbitrary values in the
+        # distogram for these regions.
+        act *= template_mask_2d[..., None]
+
+        act = self.embedding2d(act)
+
+        act = dap.scatter(act, axis=1)
+        for idx, pair_encoder in enumerate(self.template_pair_stack):
+            act = recompute_wrapper(
+                pair_encoder,
+                act,
+                mask_2d,
+                is_recompute=self.training and idx >=
+                self.config.template_pair_stack.recompute_start_block_index)
+        act = dap.gather(act, axis=1)
+
+        act = self.output_layer_norm(act)
+        return act
+
+
+class TemplateEmbedding(nn.Layer):
+    """Embeds a set of templates.
+
+        Jumper et al. (2021) Suppl. Alg. 2 "Inference" lines 9-12
+        Jumper et al. (2021) Suppl. Alg. 17 "TemplatePointwiseAttention"
+    """
+
+    def __init__(self, channel_num, config, global_config):
+        super(TemplateEmbedding, self).__init__()
+        self.config = config
+        self.global_config = global_config
+
+        self.single_template_embedding = SingleTemplateEmbedding(
+            channel_num, config, global_config)
+        self.attention = Attention(
+            config.attention, global_config, channel_num['pair_channel'],
+            config.attention.key_dim, channel_num['pair_channel'])
+
+    def forward(self, query_embedding, template_batch, mask_2d):
+        """Build TemplateEmbedding module.
+
+        Arguments:
+            query_embedding: Query pair representation, shape [n_batch, N_res, N_res, c_z].
+            template_batch: A batch of template features.
+            mask_2d: Padding mask (Note: this doesn't care if a template exists,
+                unlike the template_pseudo_beta_mask).
+
+        Returns:
+            A template embedding [n_batch, N_res, N_res, c_z].
+        """
+
+        num_templates = template_batch['template_mask'].shape[1]
+
+        num_channels = (self.config.template_pair_stack.
+                        triangle_attention_ending_node.value_dim)
+
+        num_res = query_embedding.shape[1]
+
+        dtype = query_embedding.dtype
+        template_mask = template_batch['template_mask']
+        template_mask = template_mask.astype(dtype)
+
+        query_channels = query_embedding.shape[-1]
+
+        outs = []
+        for i in range(num_templates):
+            # By default, num_templates = 4
+            batch0 = {
+                k: paddle.squeeze(
+                    v.slice([1], [i], [i + 1]), axis=1)
+                for k, v in template_batch.items()
+            }
+            outs.append(
+                self.single_template_embedding(query_embedding, batch0,
+                                               mask_2d))
+
+        template_pair_repr = paddle.stack(outs, axis=1)
+
+        flat_query = paddle.reshape(
+            query_embedding, [-1, num_res * num_res, 1, query_channels])
+        flat_templates = paddle.reshape(
+            paddle.transpose(template_pair_repr, [0, 2, 3, 1, 4]),
+            [-1, num_res * num_res, num_templates, num_channels])
+
+        bias = 1e9 * (template_mask[:, None, None, None, :] - 1.)
+
+        if not self.training:
+            sb_attn = subbatch(self.attention, [0, 1], [1, 1],
+                               self.config.subbatch_size, 1)
+            emb = sb_attn(flat_query, flat_templates, bias)
+
+        else:
+            emb = self.attention(flat_query, flat_templates, bias)
+
+        emb = paddle.reshape(emb, [-1, num_res, num_res, query_channels])
+
+        # No gradients if no templates.
+        emb *= (paddle.sum(template_mask) > 0.).astype(emb.dtype)
+        return emb
