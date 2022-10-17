72fd8efa1c40d0918dc92d69fbf0e711d55219d6
xuyubin <xuyubin@baidu.com>
2022-10-17 19:39:01 +0800

    auto_add_vit_and_imagen

--- ppfleetx/models/__init__.py
+++ ppfleetx/models/__init__.py
@@ -19,7 +19,7 @@ from ppfleetx.core.module.basic_module import BasicModule
 from ppfleetx.models.language_model.language_module import GPTModule, GPTGenerationModule, GPTEvalModule, GPTFinetuneModule
 from ppfleetx.models.language_model.gpt.auto.auto_module import GPTModuleAuto, GPTGenerationModuleAuto
 from ppfleetx.models.vision_model.general_classification_module import GeneralClsModule,GeneralClsModuleAuto
-from ppfleetx.models.multimodal_model.multimodal_module import ImagenModule
+from ppfleetx.models.multimodal_model.multimodal_module import ImagenModule,ImagenModuleAuto
 from ppfleetx.models.language_model.ernie import ErnieModule
 
 from ppfleetx.models.multimodal_model.multimodal_module import ImagenModule
