5748d1bcbd5d5c3310ca20a044b76a8eebbb51d1
haohongxiang <haohongxiang@baidu.com>
2022-10-20 07:56:01 +0000

    update

--- ppfleetx/distributed/moe/utils.py
+++ ppfleetx/distributed/moe/utils.py
@@ -19,9 +19,24 @@
 #     Copyright 2021, Jiaao He. All rights reserved.
 #   Licensed under the Apache License, Version 2.0 (the "License").
 
-from paddle.distributed.models.moe.utils import _number_count, _limit_by_capacity, _prune_gate_by_capacity, _assign_pos
 import paddle
+from paddle.distributed.models.moe.utils import _number_count, _limit_by_capacity, _prune_gate_by_capacity, _assign_pos
 from paddle.fluid.framework import in_dygraph_mode
+
+
+def prepare_forward(gate, num_expert, world_size, moe_group):
+    pos, local_expert_count, global_expert_count = count_by_gate(
+        gate, num_expert, world_size, group=moe_group)
+    with paddle.no_grad():
+        fwd_expert_count = global_expert_count.reshape_(
+            [world_size, num_expert]).sum(axis=0)
+        fwd_batch_size = int(fwd_expert_count.sum().item())
+    return (
+        pos,
+        local_expert_count,
+        global_expert_count,
+        fwd_expert_count,
+        fwd_batch_size, )
 
 
 def _alltoall(in_tensor_list, group=None, use_calc_stream=True):
@@ -40,6 +55,54 @@ def _alltoall(in_tensor_list, group=None, use_calc_stream=True):
         return paddle._legacy_C_ops.alltoall(in_tensor_list, 'use_calc_stream',
                                              use_calc_stream, 'ring_id',
                                              ring_id)
+
+
+def _local_scatter(inp, pos):
+    if pos.shape != [0]:
+        inp_buf = paddle.index_select(inp, pos, 0)
+    else:
+        inp_buf = paddle.empty([0, inp.shape[1]], dtype=inp.dtype)
+    return inp_buf
+
+
+def _local_gather(inp, pos, out_batch_size, maybe_overlap=True):
+    if pos.shape != [0]:
+        origin_dtype = inp.dtype
+        inp = paddle.cast(inp, dtype="float32")
+        inp_buf = paddle.scatter(
+            paddle.zeros(
+                shape=[out_batch_size, inp.shape[-1]], dtype="float32"),
+            pos,
+            inp,
+            overwrite=True)
+        inp_buf = paddle.cast(inp_buf, dtype=origin_dtype)
+    else:
+        inp_buf = paddle.zeros(
+            [out_batch_size, inp.shape[-1]], dtype=inp.dtype)
+    return inp_buf
+
+
+def _all_gather(tensor, group=None, use_calc_stream=True):
+    if group is not None and not group.is_member():
+        return
+
+    if in_dygraph_mode():
+        group = paddle.distributed.collective._get_default_group(
+        ) if group is None else group
+        tensor_shape = list(tensor.shape)
+        tensor_shape[0] *= group.nranks
+        out = paddle.empty(tensor_shape, tensor.dtype)
+
+        task = group.process_group.all_gather(tensor, out)
+        task.wait()
+        return out
+    else:
+        ring_id = 0 if group is None else group.id
+        nranks = paddle.distributed.collective._get_global_group(
+        ).nranks if group is None else group.nranks
+        return paddle._legacy_C_ops.c_allgather(tensor, 'use_calc_stream',
+                                                use_calc_stream, 'ring_id',
+                                                ring_id, 'nranks', nranks)
 
 
 def count_by_gate(gate, num_expert, world_size, require_pos=True, group=None):
