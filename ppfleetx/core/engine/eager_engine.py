d6c48259cb970d921e6f0b22c79d278f8900ecb0
xu98bin <78574951+xu98bin@users.noreply.github.com>
2022-11-04 18:49:14 +0800

    dynamic mode aligns static mode (#879)

--- ppfleetx/core/engine/eager_engine.py
+++ ppfleetx/core/engine/eager_engine.py
@@ -35,6 +35,7 @@ from ppfleetx.core.module import BasicModule
 from ppfleetx.utils.tensor_fusion_helper import all_reduce_parameters
 from ppfleetx.utils.version import version_check
 from ppfleetx.utils.export import export_inference_model
+from paddle.incubate.distributed.utils.io import save_for_auto_inference
 
 
 class EagerEngine(BasicEngine):
@@ -661,6 +662,10 @@ class EagerEngine(BasicEngine):
                 "cuda_rng_state": paddle.get_cuda_rng_state()
             }
             paddle.save(meta_dict, os.path.join(save_dir, "meta_state.pdopt"))
+
+            save_auto_dir = os.path.join(output_dir, "auto_infer")
+            save_for_auto_inference(
+                os.path.join(save_auto_dir, "auto"), self._module.model)
 
         else:
             raise TypeError("`save` requires a valid value of `output_dir`.")
