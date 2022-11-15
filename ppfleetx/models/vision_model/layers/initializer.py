c15fe32ac1f115e6b14a430127ef246fca101f4f
sneaxiy <sneaxiy@126.com>
2022-11-15 21:25:19 +0800

    add xavier_uniform_2d

--- ppfleetx/models/vision_model/layers/initializer.py
+++ ppfleetx/models/vision_model/layers/initializer.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from paddle.nn.initializer import Constant, Normal, XavierUniform
+import numpy as np
+import math
+from paddle.nn.initializer import Constant, Normal, XavierUniform, Uniform
 
 mlp_bias_normal_ = Normal(std=1e-6)
 pos_normal_ = Normal(std=0.02)
@@ -20,3 +22,15 @@ xavier_uniform_ = XavierUniform()
 zeros_ = Constant(value=0.)
 minus_tens_ = Constant(value=-10.)
 ones_ = Constant(value=1.)
+
+
+class XavierUniform2D(XavierUniform):
+    def __call__(self, param, block=None):
+        fan_in = int(np.prod(param.shape[:-1]))
+        fan_out = param.shape[-1]
+        limit = math.sqrt(6.0 / (fan_in + fan_out))
+        uniform = Uniform(low=-limit, high=limit)
+        uniform(param)
+
+
+xavier_uniform_2d_ = XavierUniform2D()
