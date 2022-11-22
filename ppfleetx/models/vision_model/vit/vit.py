d4e1e52fce29c5dee7e2879a5e75cbcbe22cbd7b
Ghost Screaming <mofengshenjieII@163.com>
2022-11-22 12:06:00 +0800

    Improve vision transformer performance. (#915)
    * add xavier_uniform_2d
    
    * update
    
    * Improve vision transfomer performance with following methods:
    1. Set FLAGS_enable_cublas_tensor_op_math and
       FLAGS_gemm_use_half_precision_compute_type flags. These two flags
       have remarkable impact on CuBLAS BatchGEMM computation performance.
    2. Add fused attention blocks, which can break the bottleneck of vit
       performance.
    
    * Fix some bugs.
    
    * Remove DALI dataloader codes. It will be submitted in another PR.
    
    * Use '==' instead of 'is'
    
    * Fix bug of fused attention checkpoint save and load.
    
    * Put environment flags setting in config. It can be modified in yaml.
    
    * Delete useless code modification.
    
    * Polish code.
    
    * Remove useless print.
    
    * Polish code.
    
    * Fix bug of state_dict, which will cause error in save_for_auto_inference
    function.
    
    Co-authored-by: sneaxiy <sneaxiy@126.com>
    Co-authored-by: sneaxiy <32832641+sneaxiy@users.noreply.github.com>
    Co-authored-by: Guoxia Wang <mingzilaochongtu@gmail.com>

--- ppfleetx/models/vision_model/vit/vit.py
+++ ppfleetx/models/vision_model/vit/vit.py
@@ -20,13 +20,17 @@ import numpy as np
 import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
+from paddle.incubate.nn import FusedMultiHeadAttention, FusedFeedForward
 
+from ppfleetx.utils.log import logger
 from ..layers.droppath import DropPath
 from ..layers.identity import Identity
 from ..layers.attention import ViTAttention
 from ..layers.embedding import ViTPatchEmbed
 from ..layers.mlp import ViTMLP
-from ..layers.initializer import xavier_uniform_, zeros_, minus_tens_, pos_normal_, ones_
+from ..layers.initializer import (xavier_uniform_, xavier_uniform_2d_,
+                                  mlp_bias_normal_, zeros_, minus_tens_,
+                                  pos_normal_, ones_)
 
 __all__ = [
     'ViT_tiny_patch16_224',
@@ -45,6 +49,68 @@ __all__ = [
     'ViT_6B_patch14_224',
     'ViT',
 ]
+
+
+class FusedBlock(nn.Layer):
+    def __init__(self,
+                 dim,
+                 num_heads,
+                 mlp_ratio=4.,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 drop=0.,
+                 attn_drop=0.,
+                 drop_path=0.,
+                 act_layer=nn.GELU,
+                 norm_layer='nn.LayerNorm',
+                 epsilon=1e-5):
+        super().__init__()
+
+        assert qk_scale is None, "Fused attention doesn't support qk_scale."
+        if isinstance(drop_path, (float, int)):
+            assert drop_path == 0.0, "Fused attention doesn't support drop_path."
+        elif isinstance(drop_path, (tuple, list)):
+            assert drop_path == [0.0] * len(
+                drop_path), "Fused attention doesn't support drop_path."
+        assert norm_layer == "nn.LayerNorm", "Fused attention only support nn.LayerNorm"
+        assert ((act_layer == nn.GELU) or (act_layer == nn.ReLU)) or \
+                (isinstance(act_layer, str) and act_layer.lower() == "gelu" or act_layer.lower() == "relu"), \
+                "Fused attention only support GELU and ReLU activation."
+
+        self.attn = FusedMultiHeadAttention(
+            dim,
+            num_heads=num_heads,
+            qkv_bias_attr=qkv_bias,
+            dropout_rate=drop,
+            attn_dropout_rate=attn_drop,
+            normalize_before=True,
+            epsilon=epsilon)
+
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        if (act_layer == nn.GELU) or act_layer.lower() == "gelu":
+            act_func = "gelu"
+        else:
+            act_func = "relu"
+        self.mlp = FusedFeedForward(
+            d_model=dim,
+            dim_feedforward=mlp_hidden_dim,
+            dropout_rate=drop,
+            activation=act_func,
+            act_dropout_rate=drop,
+            normalize_before=True)
+
+        xavier_uniform_2d_(self.attn.qkv_weight)
+        xavier_uniform_2d_(self.attn.linear_weight)
+        xavier_uniform_2d_(self.mlp._linear1_weight)
+        xavier_uniform_2d_(self.mlp._linear2_weight)
+
+        zeros_(self.attn.qkv_bias)
+        zeros_(self.attn.linear_bias)
+        mlp_bias_normal_(self.mlp._linear1_bias)
+        mlp_bias_normal_(self.mlp._linear2_bias)
+
+    def forward(self, x):
+        return self.mlp(self.attn(x))
 
 
 class Block(nn.Layer):
@@ -118,11 +184,12 @@ class ViT(nn.Layer):
                  norm_layer='nn.LayerNorm',
                  epsilon=1e-5,
                  representation_size=None,
+                 use_fused_attn=False,
                  **kwargs):
         super().__init__()
         self.class_num = class_num
         self.representation_size = representation_size
-
+        self.num_heads = num_heads
         self.num_features = self.embed_dim = embed_dim
 
         self.patch_embed = ViTPatchEmbed(
@@ -140,8 +207,15 @@ class ViT(nn.Layer):
 
         dpr = np.linspace(0, drop_path_rate, depth)
 
+        self.use_fused_attn = use_fused_attn
+        block_fn = FusedBlock if self.use_fused_attn else Block
+        if self.use_fused_attn:
+            logger.info(
+                "ViT use fused attention. Fused attention model checkpoint will be" \
+                " saved in normal attention format for inference checkpoint export," \
+                " and its optimizer checkpoint keeps the same.")
         self.blocks = nn.LayerList([
-            Block(
+            block_fn(
                 dim=embed_dim,
                 num_heads=num_heads,
                 mlp_ratio=mlp_ratio,
@@ -205,6 +279,92 @@ class ViT(nn.Layer):
         x = self.head(x)
         return x
 
+    # Saved the fused attention checkpoint in origin attention checkpoint format
+    replaced_dict = {
+        # FusedMultiHeadAttention
+        'attn.pre_ln_scale': 'norm1.weight',
+        'attn.pre_ln_bias': 'norm1.bias',
+        'attn.qkv_weight': 'attn.qkv.weight',
+        'attn.qkv_bias': 'attn.qkv.bias',
+        'attn.linear_weight': 'attn.proj.weight',
+        'attn.linear_bias': 'attn.proj.bias',
+        # FusedFeedForward
+        'mlp._ln1_scale': 'norm2.weight',
+        'mlp._ln1_bias': 'norm2.bias',
+        'mlp._linear1_weight': 'mlp.fc1.weight',
+        'mlp._linear1_bias': 'mlp.fc1.bias',
+        'mlp._linear2_weight': 'mlp.fc2.weight',
+        'mlp._linear2_bias': 'mlp.fc2.bias',
+    }
+
+    @paddle.no_grad()
+    def state_dict(self,
+                   destination=None,
+                   include_sublayers=True,
+                   structured_name_prefix="",
+                   use_hook=True):
+        state_dict = super().state_dict(destination, include_sublayers,
+                                        structured_name_prefix, use_hook)
+        if self.use_fused_attn:
+            new_dict = []
+            poped_keys = []
+            for key, value in state_dict.items():
+                new_key = ""
+                for k, v in self.replaced_dict.items():
+                    if k in key:
+                        new_key = key.replace(k, v)
+                        break
+                if new_key != "":
+                    value_name = value.name
+                    if 'attn.qkv.weight' in new_key:
+                        value = value.reshape([-1, value.shape[-1]]).transpose(
+                            [1, 0])
+                    if 'attn.qkv.bias' in new_key:
+                        value = value.reshape([-1])
+                    # value is a Tensor after transformation,
+                    # it will be transformed to ParamBase for auto_infer
+                    param = paddle.create_parameter(
+                        shape=value.shape, dtype=value.dtype)
+                    param.set_value(value)
+                    param.name = value_name
+                    new_dict.append({new_key: param})
+                    poped_keys.append(key)
+
+            for i in range(len(new_dict)):
+                state_dict.update(new_dict[i])
+                state_dict.pop(poped_keys[i])
+        return state_dict
+
+    @paddle.no_grad()
+    def set_state_dict(self, state_dict, use_structured_name=True):
+        reversed_replaced_dict = {}
+        for k, v in self.replaced_dict.items():
+            reversed_replaced_dict.update({v: k})
+
+        if self.use_fused_attn:
+            new_dict = []
+            poped_keys = []
+            for key, value in state_dict.items():
+                new_key = ""
+                for k, v in reversed_replaced_dict.items():
+                    if k in key:
+                        new_key = key.replace(k, v)
+                        break
+                if new_key != "":
+                    if 'attn.qkv_weight' in new_key:
+                        value = value.transpose([1, 0])
+                        value = value.reshape(
+                            [3, self.num_heads, -1, value.shape[-1]])
+                    if 'attn.qkv_bias' in new_key:
+                        value = value.reshape([3, self.num_heads, -1])
+                    new_dict.append({new_key: value})
+                    poped_keys.append(key)
+
+            for i in range(len(new_dict)):
+                state_dict.update(new_dict[i])
+                state_dict.pop(poped_keys[i])
+        super().set_state_dict(state_dict)
+
     def load_pretrained(self, prefix_path, finetune=False):
         if not os.path.exists(prefix_path + '.pdparams'):
             raise ValueError("Model pretrain path {} does not "
@@ -219,7 +379,7 @@ class ViT(nn.Layer):
                 paddle.float32)
 
         if not finetune:
-            self.set_dict(param_state_dict)
+            self.set_state_dict(param_state_dict)
             return
 
         for k in ['head0.weight', 'head0.bias', 'head.weight', 'head.bias']:
@@ -255,8 +415,9 @@ class ViT(nn.Layer):
         new_pos_embed = paddle.concat((extra_tokens, pos_tokens), axis=1)
         param_state_dict['pos_embed'] = new_pos_embed
 
-        self.set_dict(param_state_dict)
+        self.set_state_dict(param_state_dict)
         return
+
 
 def ViT_tiny_patch16_224(**kwargs):
     model = ViT(patch_size=16,
@@ -269,6 +430,7 @@ def ViT_tiny_patch16_224(**kwargs):
                 representation_size=192,
                 **kwargs)
     return model
+
 
 def ViT_base_patch16_224(**kwargs):
     model = ViT(patch_size=16,
@@ -384,6 +546,8 @@ def ViT_huge_patch14_224(**kwargs):
                 depth=32,
                 num_heads=16,
                 mlp_ratio=4,
+                qkv_bias=True,
+                epsilon=1e-6,
                 representation_size=1280,
                 **kwargs)
     return model
@@ -396,6 +560,8 @@ def ViT_huge_patch14_384(**kwargs):
                 depth=32,
                 num_heads=16,
                 mlp_ratio=4,
+                qkv_bias=True,
+                epsilon=1e-6,
                 representation_size=None,
                 **kwargs)
     return model
