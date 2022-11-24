ac8982fdfb934d7738e38810e648e86f256fc1f0
minghaoBD <liminghao03@baidu.com>
2022-11-24 12:18:27 +0000

    almost done with few hardcode

--- eval.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=3
-
-# python3.7 ./tools/eval.py -c ./ppfleetx/configs/nlp/gpt/eval_gpt_6.7B_single_card_prune.yaml
-python3.7 ./tools/eval.py -c ppfleetx/configs/nlp/gpt/eval_gpt_345M_single_card_prune_quant.yaml
-
--- export.sh
+++
@@ -1,4 +0,0 @@
-export CUDA_VISIBLE_DEVICES=7
-
-python3.7 tools/export.py \
-    -c ppfleetx/configs/nlp/gpt/inference_gpt_345M_single_card.yaml
