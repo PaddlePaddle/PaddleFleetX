c2e0218ad876b6645bce1c6aa7a47e49c3a560c3
fanghuaqiang <vivienfanghua@163.com>
2022-11-29 01:46:44 +0000

    fix some errors of gpt benchmarks

--- projects/gpt/run_benchmark.sh
+++ projects/gpt/run_benchmark.sh
@@ -1,8 +1,8 @@
 
 # for mp=8(GPT 175b)
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python -m -m paddle.distributed.launch --devices "0,1,2,3,4,5,6,7" projects/gpt/benchmark.py --seq_len 128 --iter 10 --mp_size 8 --model_dir ./output
+python -m paddle.distributed.launch --devices "0,1,2,3,4,5,6,7" projects/gpt/benchmark.py --seq_len 128 --iter 10 --mp_size 8 --model_dir ./output
 
 # for mp=1(GPT 6.7B & GPT 345M)
 export CUDA_VISIBLE_DEVICES=0
-python -m -m paddle.distributed.launch --devices "0" projects/gpt/benchmark.py --seq_len 128 --iter 10 --mp_size 1 --model_dir ./output
+python -m paddle.distributed.launch --devices "0" projects/gpt/benchmark.py --seq_len 128 --iter 10 --mp_size 1 --model_dir ./output
