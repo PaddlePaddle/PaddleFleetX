730046edfb0d51f757a05032feb759b573f84c91
Roc <lrslj@live.com>
2022-11-29 09:07:57 +0000

    update

--- run_gpt.sh
+++ run_gpt.sh
@@ -1,22 +1,22 @@
 
 #export LD_PRELOAD=/usr/local/lib/python3.7/dist-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0 # for npu
-
+# paddle commit f41ccbd549549e2f9accc467ade1b01a86eb6d2d
 export PYTHONPATH=/Paddle/Paddle/build2/python:/Paddle/fleetx/PaddleSlim:$PYTHONPATH
 export BKCL_PCIE_RING=1
 
-export XPU_VISIBLE_DEVICES=4,5,6,7 #比如x=0
+export XPU_VISIBLE_DEVICES=4 #比如x=0
 rm -rf log_gpt
 python -m paddle.distributed.launch \
 --devices ${XPU_VISIBLE_DEVICES} --log_dir log_gpt \
 tools/train.py \
 -c ppfleetx/configs/nlp/gpt/pretrain_gpt_345M_single_card.yaml \
 -o Engine.mix_precision.use_pure_fp16="False" \
--o Distributed.dp_degree=2 \
+-o Distributed.dp_degree=1 \
 -o Distributed.mp_degree=1 \
 -o Distributed.pp_degree=1 \
--o Distributed.sharding.sharding_degree=2 \
--o Distributed.sharding.sharding_stage=2 \
--o Model.use_recompute="False" \
+-o Distributed.sharding.sharding_degree=1 \
+-o Distributed.sharding.sharding_stage=1 \
+-o Model.use_recompute="True" \
 -o Global.local_batch_size=2 \
 -o Global.micro_batch_size=2 \
 -o Global.device=xpu \
