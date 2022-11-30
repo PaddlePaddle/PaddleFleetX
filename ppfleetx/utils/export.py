c1b63ea7e5dfdb966395d042f9789f88fbf1e407
minghaoBD <liminghao03@baidu.com>
2022-11-30 06:20:28 +0000

    remove print

--- ppfleetx/utils/export.py
+++ ppfleetx/utils/export.py
@@ -56,7 +56,6 @@ def export_inference_model(
                                           static_model.forward.main_program,
                                           static_model.forward.outputs)
 
-    print(pruned_input_spec)
     if export_quant_model:
         quanter.save_quantized_model(
             model,
