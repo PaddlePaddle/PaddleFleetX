c91472dd663947652203d9ee420518fbec569c18
haohongxiang <haohongxiang@baidu.com>
2023-03-02 11:01:00 +0000

    fix format

--- ppfleetx/models/language_model/language_module.py
+++ ppfleetx/models/language_model/language_module.py
@@ -75,10 +75,12 @@ class LanguageModule(BasicModule):
         default_global_tokens_num = self.configs.Global.global_batch_size * \
             self.configs.Data.Train.dataset.max_seq_len
 
-        loss_scale_str = "loss_scale: %.9f, ".format(log_dict['loss_scale']) if log_dict.get('loss_scale', None) is not None else ""
+        loss_scale_str = "loss_scale: %.9f,".format(log_dict[
+            'loss_scale']) if log_dict.get('loss_scale',
+                                           None) is not None else ""
         logger.info(
             "[train] epoch: [%d/%d], batch: [%d/%d], loss: %.9f, avg_batch_cost: %.5f sec, speed: %.2f step/s, " \
-            "ips_total: %.0f tokens/s, ips: %.0f tokens/s, {}learning rate: %.5e"
+            "ips_total: %.0f tokens/s, ips: %.0f tokens/s, %s learning rate: %.5e"
             % (log_dict['epoch'], log_dict['total_epoch'], log_dict['batch'], log_dict['total_step'], log_dict['loss'],
                log_dict['train_cost'], speed, speed * default_global_tokens_num, speed * default_global_tokens_num / self.data_world_size, loss_scale_str, log_dict['lr']))
 
