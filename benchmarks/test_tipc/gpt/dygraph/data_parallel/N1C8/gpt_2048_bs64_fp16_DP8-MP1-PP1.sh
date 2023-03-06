4488f4ad013e4621f8960ee51de7cbd190d98eae
Liujie0926 <44688141+Liujie0926@users.noreply.github.com>
2023-03-06 20:51:08 +0800

    [benchmarks]update case for gpt&imagen (#1056)
    * [Benchmark] fix imagen case name
    
    * fix gpt_bug & update imagen_step
    
    * add gpt case

--- benchmarks/test_tipc/gpt/dygraph/data_parallel/N1C8/gpt_2048_bs64_fp16_DP8-MP1-PP1.sh
+++ benchmarks/test_tipc/gpt/dygraph/data_parallel/N1C8/gpt_2048_bs64_fp16_DP8-MP1-PP1.sh
@@ -12,24 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-model_item=imagen_2B_text2im_64
-dp_degree=1
+model_item=gpt_2048
+dp_degree=8
 mp_degree=1
 pp_degree=1
-bs_item=8
-fp_item=fp32
-run_mode=DP1-Sharding8
+bs_item=64
+fp_item=fp16
+run_mode=DP8-MP1-PP1
 device_num=N1C8
-yaml_path=ppfleetx/configs/multimodal/imagen/imagen_text2im_64x64_T5-11B.yaml
-max_iter=1000
-sharding_degree=8
-sharding_stage=2
+yaml_path=./ppfleetx/configs/nlp/gpt/pretrain_gpt_1.3B_dp8.yaml
 
-model=imagen
-micro_bs=1
+model=gpt
+micro_bs=8
 
 cd ./benchmarks
-bash ./test_tipc/imagen/dygraph/benchmark_common/prepare.sh
+bash ./test_tipc/gpt/dygraph/data_parallel/benchmark_common/prepare.sh
 # run
-bash ./test_tipc/imagen/dygraph/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${dp_degree} ${mp_degree} ${pp_degree} ${micro_bs} ${bs_item} ${run_mode} ${device_num} \
-${yaml_path} ${max_iter} ${sharding_degree} ${sharding_stage} 2>&1;
+bash ./test_tipc/gpt/dygraph/data_parallel/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${dp_degree} ${mp_degree} ${pp_degree} ${micro_bs} ${bs_item} ${run_mode} ${device_num} \
+${yaml_path} 2>&1;
