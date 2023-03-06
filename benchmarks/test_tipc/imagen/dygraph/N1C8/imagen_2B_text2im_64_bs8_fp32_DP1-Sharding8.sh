988cf7564c9d3d1dd67717293fa09b16893590a6
liujie44 <liujie44@baidu.com>
2023-03-06 19:30:52 +0800

    fix gpt_bug & update imagen_step

--- benchmarks/test_tipc/imagen/dygraph/N1C8/imagen_2B_text2im_64_bs8_fp32_DP1-Sharding8.sh
+++ benchmarks/test_tipc/imagen/dygraph/N1C8/imagen_2B_text2im_64_bs8_fp32_DP1-Sharding8.sh
@@ -22,7 +22,6 @@ run_mode=DP1-Sharding8
 device_num=N1C8
 yaml_path=ppfleetx/configs/multimodal/imagen/imagen_text2im_64x64_T5-11B.yaml
 max_iter=1000
-logging_freq=1
 sharding_degree=8
 sharding_stage=2
 
@@ -33,4 +32,4 @@ cd ./benchmarks
 bash ./test_tipc/imagen/dygraph/benchmark_common/prepare.sh
 # run
 bash ./test_tipc/imagen/dygraph/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${dp_degree} ${mp_degree} ${pp_degree} ${micro_bs} ${bs_item} ${run_mode} ${device_num} \
-${yaml_path} ${max_iter} ${logging_freq} ${sharding_degree} ${sharding_stage} 2>&1;
+${yaml_path} ${max_iter} ${sharding_degree} ${sharding_stage} 2>&1;
